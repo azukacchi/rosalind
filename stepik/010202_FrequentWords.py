7dd300dff163abf62830e3106a1d802b2e461c0f
Azka <66941381+azukacchi@users.noreply.github.com>
2021-11-05 19:36:52 +0700

    Create 010202_FrequentWords.py

---
+++ stepik/010202_FrequentWords.py
@@ -0,0 +1,20 @@
+import re
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+with open(os.path.join(BASE_DIR, 'dataset_240214_13.txt')) as f:
+    (s,k) = (i for i in f.read().strip().split())
+
+k = int(k)
+# s = 'ACGTTGCATGTCGCATGATGCATGAGAGCT'
+# k = 4
+
+kmer = {}
+
+for i in [s[index : index + k] for index in range(0, len(s))]:
+    if i not in kmer:
+        kmer[i] = 1
+    else: kmer[i] += 1
+
+max_value = max(kmer.values())
+print(' '.join([key for key, value in kmer.items() if value == max_value]))
