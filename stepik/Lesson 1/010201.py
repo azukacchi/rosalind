64d87d6b40892d7097aa79f9a07bb3b4c678e130
Azka <66941381+azukacchi@users.noreply.github.com>
2021-11-05 19:08:58 +0700

    Create 010201.py

---
+++ stepik/Lesson 1/010201.py
@@ -0,0 +1,7 @@
+import re
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+with open(os.path.join(BASE_DIR, 'dataset_240214_6.txt')) as f:
+    (s,t) = (i for i in f.read().strip().split())
+print(len([str(m.start()+1) for m in re.finditer(f'(?={t})', s)]))
